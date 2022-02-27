e74d3709cfa9696af7d8cacba6c461bec0845529
unknown <lh77x7@gmail.com>
2022-02-27 10:31:36 +0100

    exc 5 chap 10
    exercise 5 chapter 10 - done

---
+++ ch10/exc/exc5/Stack.h
@@ -0,0 +1,25 @@
+// Stack.h -- class definition for Stack ADT (rozwiazanie zadania 5 rozdział 10)
+
+#ifndef STACK_H_
+#define STACK_H_
+
+struct customer{
+    char fullname[35];
+    double payment;
+};
+
+typedef customer Item;
+
+class Stack{
+    private:
+        enum{ MAX = 10 };
+        Item items[MAX];
+        int top;
+    public:
+        Stack();
+        bool isempty() const;
+        bool isfull() const;
+        bool push(const Item & item);
+        bool pop(Item & item);
+};
+#endif
