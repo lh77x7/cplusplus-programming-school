2acaa76621a5883f671a4e818f61ce0c2a442361
unknown <lh77x7@gmail.com>
2022-03-03 10:47:39 +0100

    exc 7 chap 10
    exercise 7 chapter 10 - done

---
+++ ch10/exc/exc7/plorg.h
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class Plorg
+{
+private:
+	char Myname[19];
+	int MyCI;
+public:
+	Plorg(const char*name = "Plorga", int consIndex = 50);
+	void Show()const; //cout object data 
+};
---
+++ ch10/exc/exc7/useplorg.cpp
@@ -0,0 +1,9 @@
+#include "plorg.h"
+int main()
+{
+    Plorg one, two("Zeland", 100);
+    one.Show();
+    two.Show();
+
+    return 0;
+}
