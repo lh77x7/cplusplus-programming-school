22f0a5960c905bc9fcf91eac13703e5b17fdb78e
unknown <lh77x7@gmail.com>
2022-03-04 12:27:22 +0100

    exa 2 chap 11
    example 2 chapter 11 - done

---
+++ ch11/usetime1.cpp
@@ -0,0 +1,44 @@
+// usetime1.cpp -- test drugiej wersji klasy Time
+// kompilować z plikiem mytime1.cpp
+#include <iostream>
+#include "mytime1.h"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    Time planning;
+    Time coding(2, 40);
+    Time fixing(5, 55);
+    Time total;
+
+    cout << "czas planowania = ";
+    planning.Show();
+    cout << endl;
+
+    cout << "czas kodowania = ";
+    coding.Show();
+    cout << endl;
+
+    cout << "czas poprawiania = ";
+    fixing.Show();
+    cout << endl;
+
+    // zapis operatorowy
+    total = coding + fixing;
+    cout << "lacznie (coding + fixing) = ";
+    total.Show();
+    cout << endl;
+
+    Time morefixing(3, 28);
+    cout << "kolejne poprawki = ";
+    morefixing.Show();
+    cout << endl;
+    total = morefixing.operator+(total);
+    // zapis funkcyjny
+    cout << "lacznie (morefixing.operator+(total)) = ";
+    total.Show();
+    cout << endl;
+
+    return 0;
+}
