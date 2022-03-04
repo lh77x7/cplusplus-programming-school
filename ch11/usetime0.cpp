64e3aeacb8ef86c08e2d2212a70e15130e5a3505
unknown <lh77x7@gmail.com>
2022-03-04 11:43:02 +0100

    exa 1 chap 11
    example 1 chapter 11 - done

---
+++ ch11/usetime0.cpp
@@ -0,0 +1,32 @@
+// usetime0.cpp -- test pierwszej klasy Time
+// kompilowac razem z plikiem mytime0.cpp 
+#include <iostream>
+#include "mytime0.h"
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    Time planning;
+    Time coding(2, 40);
+    Time fixing(5, 55);
+    Time total;
+
+    cout << "czas planowania = ";
+    planning.Show();
+    cout << endl;
+
+    cout << "czas kodowania = ";
+    coding.Show();
+    cout << endl;
+
+    cout << "czas poprawiania = ";
+    fixing.Show();
+    cout << endl;
+    total = coding.Sum(fixing);
+    cout << "lacznie (coding.Sum(fixing)) = ";
+    total.Show();
+    cout << endl;
+
+    return 0;
+}
