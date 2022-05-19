8e3cfe66f5093d73321befde56f4254eac551f96
lh77x7 <lh77x7@gmail.com>
2022-05-19 21:59:17 +0200

    exa 7 chap 16

---
+++ ch16/vect1.cpp
@@ -0,0 +1,36 @@
+// vect1.cpp -- przedstawienie podstawowych funkcji szablonu vector
+#include <iostream>
+#include <string>
+#include <vector>
+
+const int NUM = 5;
+int main()
+{
+    using std::vector;
+    using std::string;
+    using std::cin;
+    using std::cout;
+    using std::endl;
+
+    vector<int> ratings(NUM);
+    vector<string> titles(NUM);
+    cout << "Postepuj dokladnie wedlug instrukcji. Wpisz\n"
+        << NUM << " tytulow ksiazek oraz ich oceny na skali 0-10.\n";
+    int i;
+    for(i = 0; i < NUM; i++)
+    {
+        cout << "Wpisz tutul numer " << i + 1 << ": ";
+        getline(cin, titles[i]);
+        cout << "Wspisz ocene na skali 0-10: ";
+        cin >> ratings[i];
+        cin.get();
+    }
+
+    cout << "Dziekuje. Wpisales nastepujace dane:\n"
+        << "Ocena\tKsiazka\n";
+    for(i = 0; i < NUM; i++)
+    {
+        cout << ratings[i] << "\t" << titles[i] << endl;
+    }
+    return 0;
+}
