1656687cd2bcc70b39f0c8b4e4e99ac1a3e97492
lh77x7 <lh77x7@gmail.com>
2022-05-21 00:09:58 +0200

    exa 9 chap 16

--- ch16/vect3.cpp
+++ ch16/vect3.cpp
@@ -25,11 +25,66 @@ while (FillReview(temp))
     books.push_back(temp);
 if(books.size() > 0)
 {
-    cout << "Dziekuje";
+    cout << "Dziekuje. Podales "
+        << books.size() << " ocen ksiazek:\n"
+        << "Ocena\tKsiazka\n";
+    for_each(books.begin(), books.end(), ShowReview);
+
+    sort(books.begin(), books.end());
+    cout << "Posortowane wedlug tytulu:\nOcena\tKsiazka\n";
+    for_each(books.begin(), books.end(), ShowReview);
+
+    sort(books.begin(), books.end(), worseThan);
+    cout << "Posortowane wedlug oceny:\nOcena\tKsiazka\n";
+    for_each(books.begin(), books.end(), ShowReview);
+
+    random_shuffle(books.begin(), books.end());
+    cout << "Po wymieszaniu:\nOcena\tKsiazka\n";
+    for_each(books.begin(), books.end(), ShowReview);
 }
 else
     cout << "Brak danych";
 cout << "Koniec\n";
 
 return 0;
+}
+
+bool operator<(const Review & r1, const Review & r2)
+{
+    if(r1.title < r2.title)
+        return true;
+    else if (r1.title == r2.title && r1.rating < r2.rating)
+        return true;
+    else
+        return false;
+}
+
+bool worseThan(const Review & r1, const Review & r2)
+{
+    if(r1.rating < r2.rating)
+        return true;
+    else
+        return false;
+}
+
+bool FillReview(Review & rr)
+{
+    std::cout << "Wpisz tytul ksiazki (koniec, aby zakonczyc): ";
+    std::getline(std::cin, rr.title);
+    if(rr.title == "koniec")
+        return false;
+    std::cout << "Wpisz ocene: ";
+    std::cin >> rr.rating;
+    if(!std::cin)
+        return false;
+    // pozbycie się reszty danych
+    while(std::cin.get() != '\n')
+        continue;
+
+    return true; 
+}
+
+void ShowReview(const Review & rr)
+{
+    std::cout << rr.rating << "\t" << rr.title << std::endl;
 }
