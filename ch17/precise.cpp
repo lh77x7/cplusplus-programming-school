ff316866989c593beb681213f16b34652461591f
lh77x7 <lh77x7@gmail.com>
2022-05-23 22:07:19 +0200

    exa 6 chap 17

---
+++ ch17/precise.cpp
@@ -0,0 +1,18 @@
+// precise.cpp -- ustawienie precyzji
+#include <iostream>
+
+int main()
+{
+    using std::cout;
+    float price1 = 20.40;
+    float price2 = 1.9 + 8.0 / 9.0;
+
+    cout << "\"Futrzaki\" kosztuja " << price1 << " zl!\n";
+    cout << "\"Smoki\" kosztuja " << price2 << " zl!\n";
+
+    cout.precision(2);
+    cout << "\"Futrzaki\" kosztuja " << price1 << " zl!\n";
+    cout << "\"Smoki\" kosztuja " << price2 << " zl!\n";
+
+    return 0;
+}
