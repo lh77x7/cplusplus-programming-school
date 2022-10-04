d83a332d6f210c16d0531684e2550c32dd420caa
lh77x7 <lh77x7@gmail.com>
2022-10-04 18:22:22 +0200

    binary search program

---
+++ algoritms/chap3 - analiza sprawności algorytmów/binary_search.cpp
@@ -0,0 +1,19 @@
+int binary_search(int *tab, int x, int left, int right)
+{
+    if(left == right)
+        if(t[left] == x)
+            return left;
+        else            // element znaleziony
+            return -1;  // element nie odnaleziony
+    else
+        {
+            int mid = (left + right) / 2;
+            if(tab[mid] == x)
+                return mid;     // element znaleziony !
+            else
+                if(x < tab[mid])
+                    return szukaj_rec(tab, x, left, mid);
+                else   
+                    return szukaj_rec(tab, x, mid, right);
+        } 
+}
