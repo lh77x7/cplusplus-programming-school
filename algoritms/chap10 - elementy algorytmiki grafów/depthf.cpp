f2479d326fa7ccc8427cd3614eaf6b88a64a7571
lh77x7 <lh77x7@gmail.com>
2022-10-21 17:16:02 +0200

    depthf program

--- algoritms/chap10 - elementy algorytmiki grafów/depthf.cpp
+++ algoritms/chap10 - elementy algorytmiki grafów/depthf.cpp
@@ -1,25 +1,68 @@
 // strategia "w głąb"
 
-int i, j, G[n][n], V[n];
-// G - graf nxn
-// V - przechowuje informacje, czy dany wierzechołek 
-// był już badany (1) lub nie(0)
+#include<iostream>
+#include<vector>
+#include<stack>
 
-void szukaj(int G[n][n], in V[n])
+using namespace std;
+ 
+struct graf{
+  bool stos; //czy dodany na stos
+ 
+  vector <int> polaczenia; //do przechowywania połączeń
+  // dodatkowe opcje
+} *w; 
+ 
+void odwiedz(int n)
 {
-    int i;
-    for(i = 0; i < n; i++)
-    V[i] = 0;   // wierzchołek nie był jeszcze badany
-    for(i = 0; i < n; i++)
-        if(V[i] == 0)
-            zwiedzaj(G, V, i);
+  //wykonaj jakies czynnosci
+  //w przypadku odwiedzenia wierzcholka o numerze n
+  cout << "Odwiedzono wierzchołek o numerze: " << n << endl;
 }
-
-void zwiedzaj(int G[n][n], int V[n], int i)
+ 
+void DFS(int n)
 {
-    V[i] = 1;   // zaznaczamy wierzchołek jako "zbadany"
-    for(int k = 0; k < n; k++)
-    if(G[i][k] != 0)    // istnieje przejście
-        if(V[k] == 0)
-            zwiedzaj(G, V, k);
+  stack<int> stos;    //utworzenie stosu 
+  stos.push(n);  //dodanie pierwszego wierzcholka na stos
+ 
+  while(!stos.empty()) //dopóki jest cos na stosie
+  {
+    n = stos.top(); //pobranie elementu ze stosu
+ 
+      stos.pop(); //usuń pobrany element ze stosu
+      odwiedz(n); //odwiedź go i zrób coś
+      //oraz dodaj wszystkie jego nieodwiedzone i nie będące 
+      //na stosie połączenia na stos
+      for(int i=0; i<w[n].polaczenia.size(); i++)
+        if(!w[w[n].polaczenia[i]].stos)
+        {
+          stos.push(w[n].polaczenia[i]);
+          w[w[n].polaczenia[i]].stos = 1; //oznaczenie, że dodano na stos
+          cout << "Dodano na stos wierzcholek nr " << w[n].polaczenia[i] << endl;
+        }
+  }   
+}
+ 
+int main()
+{
+  cout << "Podaj liczbę wierzchołków w grafie: ";
+  int n, p, a, b;
+  cin >> n;
+  w = new graf [n+1];//przydzielenie pamięci na wierzchołki grafu
+  //wczytanie wierzchołków grafu
+  cout << "Podaj liczbę połączeń: ";
+  cin >> p;
+ 
+  for(int i=0; i<p; i++)
+  {
+    cout<<"Podaj numery wierzchołków, które chcesz ze sobą połączyć: ";
+    cin >> a >> b;
+    w[a].polaczenia.push_back(b); //połączenie jest dwukierunkowe a-->b
+    w[b].polaczenia.push_back(a); //b-->a
+  }
+  //przeszukaj graf
+  DFS(1); //rozpoczynamy od wierzchołka o numerze 1
+ 
+  delete [] w;
+  return 0;
 }
