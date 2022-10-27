e9314bbf7cf56516d9cf593b874fb26208cad437
lh77x7 <lh77x7@gmail.com>
2022-10-27 20:16:06 +0200

    binary program

---
+++ algoritms/chap7 - algorytmy przeszukiwania/binary-i.cpp
@@ -0,0 +1,19 @@
+int szukaj(int tab[], int x)
+{
+    enum {TAK, NIE} Znalazlem = NIE;
+    int left = 0, right = n - 1, mid;
+    while(left <= right && Znalazlem != TAK)
+    {
+        mid = (left + right) / 2;
+        if(tab[mid] == x)
+            Znalazlem = TAK;
+        else
+            if(tab[mid] < x)
+                left = mid + 1;
+            else
+                right = mid - 1;
+    }
+    if(Znalazlem == TAK)
+    return mid;
+    else return -1;
+}
